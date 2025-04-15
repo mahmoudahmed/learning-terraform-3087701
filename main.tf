abfeb9080a8071519a8b55c305fb08953041e6c4
Mahmoud Ahmed <mahmoudahmed@users.noreply.github.com>
2025-04-15 12:49:23 -0400

    Updated

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "blog_sg" {
   name = "blog"
 
   ingress_rules = ["https-443-tcp", "http-80-tcp"]
-  ingress_cidr  = ["0.0.0.0/0"]
+  ingress_cidr_blocks  = ["0.0.0.0/0"]
   engress_rules = ["all-all"]
-  engress_cidr  = ["0.0.0.0/0"]
+  engress_cidr_blocks = ["0.0.0.0/0"]
 }
